32dd6e7f918d2655a1b665dcd30fd6e8268c016b
“zeyneb <zeynebkias@outlook.fr>
2022-04-13 02:33:21 +0100

    suite

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,13 +3,16 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 import { BrowserRouter } from "react-router-dom";
+import { ContextProvider } from "./context/Context";
+
 
 
 ReactDOM.render(
   <React.StrictMode>
    <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+   <ContextProvider>
+      <App />
+    </ContextProvider>  </BrowserRouter>,
   </React.StrictMode>,
   document.getElementById('root')
 )
